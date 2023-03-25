60cbe3e90fa9ce85a02c6ceeb22e267eed7d5e23
QuangBV <quangbv2@rikkeisoft.com>
2023-03-25 14:39:26 +0900

    first commit

---
+++ terraform/3-subnets.tf
@@ -0,0 +1,49 @@
+# Resource: aws_subnet
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+resource "aws_subnet" "private-ap-northeast-1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    "Name"                            = "private-ap-northeast-1a"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+
+resource "aws_subnet" "private-ap-northeast-1c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    "Name"                            = "private-ap-northeast-1c"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+
+resource "aws_subnet" "public-ap-northeast-1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    "Name"                       = "public-ap-northeast-1a"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
+
+resource "aws_subnet" "public-ap-northeast-1c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    "Name"                       = "public-ap-northeast-1c"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
